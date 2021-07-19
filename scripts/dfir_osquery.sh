5d5404fdb4a8970bdd12df363361b9fa2fe6ac17
Your Name <you@example.com>
2021-07-19 16:35:01 +0300

    init

---
+++ scripts/dfir_osquery.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -euo pipefail
+
+osqueryi --json 'select cpu_type, cpu_brand, hardware_vendor, hardware_model from system_info;' > system_info.json
+cat system_info.json
---
+++ scripts/install_osquery.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -euo pipefail
+
+echo "===================================================================================="
+                          hostnamectl status
+echo "===================================================================================="
+echo "         \   ^__^                                                                  "
+echo "          \  (oo)\_______                                                          "
+echo "             (__)\       )\/\                                                      "
+echo "                 ||----w |                                                         "
+echo "                 ||     ||                                                         "
+
+
+
+echo "=============Installing Osquery debian arm64=============="
+export OSQUERY_KEY=1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $OSQUERY_KEY
+
+add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
+
+apt-get update -qq
+apt-get install osquery -qqy
+
+osqueryctl status osqueryd
+# osqueryctl start osqueryd
+# osqueryctl status osqueryd
+
