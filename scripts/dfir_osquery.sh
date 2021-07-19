c07e4b8658e9e11350e61b163d2b0b1ea7ec9f32
Your Name <you@example.com>
2021-07-19 17:12:30 +0300

    new_processes_listening_network_ports

--- scripts/dfir_osquery.sh
+++ scripts/dfir_osquery.sh
@@ -6,5 +6,15 @@ set -o nounset
 set -o xtrace
 # set -euo pipefail
 
+echo "===================================================================================="
+echo "get systeminfo"
+echo "===================================================================================="
 osqueryi --json 'select cpu_type, cpu_brand, hardware_vendor, hardware_model from system_info;' > system_info.json
 cat system_info.json
+
+echo "===================================================================================="
+echo "Finding new processes listening on network ports"
+echo "malware listens on port to provide command and control (C&C) or direct shell access,query periodically and diffing with the last known good "
+echo "===================================================================================="
+osqueryi --json 'SELECT DISTINCT process.name, listening.port, listening.address, process.pid FROM processes AS process JOIN listening_ports AS listening ON process.pid = listening.pid;' > new_processes_listening_network_ports.json
+cat new_processes_listening_network_ports.json
