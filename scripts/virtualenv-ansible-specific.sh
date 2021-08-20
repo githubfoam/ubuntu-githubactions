77462cfbfb13a7e06d681e2430830a1622270d7a
Your Name <you@example.com>
2021-08-20 12:03:37 +0300

    repository

--- scripts/virtualenv-ansible-specific.sh
+++ scripts/virtualenv-ansible-specific.sh
@@ -20,7 +20,15 @@ pip --version
 apt-cache policy ansible
 apt-cache show ansible
 apt-cache showpkg ansible
+
+# ansible | 2.9.6+dfsg-1 | http://azure.archive.ubuntu.com/ubuntu focal/universe amd64 Packages
 apt-cache madison ansible
+
+# https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu
+apt-get install -y software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt-cache madison ansible
+
 
 # Create a virtualenv if one does not already exist
 # python3 -m virtualenv -p $(which python3) ansible-latest 
