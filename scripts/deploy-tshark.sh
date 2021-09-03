65853ce7bbd1cae88f6b673460c9dc34d758a0c8
Your Name <you@example.com>
2021-09-03 11:01:47 +0300

    pwd

--- scripts/deploy-tshark.sh
+++ scripts/deploy-tshark.sh
@@ -5,14 +5,14 @@ set -o errexit
 set -o nounset
 set -o xtrace
 # set -euo pipefail
-
+echo "===================================================================================="
 echo "============================install tshart========================================================"
 sudo apt-get update 
 sudo apt-get install -y tshark
 
 git clone https://github.com/SecurityNik/SUWtHEh-.git
 cd "SUWtHEh-"
-
+echo "===================================================================================="
 echo "============================pcap fingerpointing========================================================"
 capinfos securitynik_kaieteur_falls.pcap  #generate a long form report
 
@@ -23,14 +23,21 @@ stat  securitynik_kaieteur_falls.csv
 
 editcap -F pcapng  -c 20  securitynik_kaieteur_falls.pcap  securitynik_kaieteur_falls_split.cap
 
+capinfos securitynik_kaieteur_falls_split.cap #generate a long form report
+
+echo "===================================================================================="
 echo "============================tshart analysis========================================================"
 
-# tshark --export-objects --help
 # Exporting suspicious/malicious content
 tshark -n -r securitynik_kaieteur_falls.pcap -q -z io,phs
 
 # Export suspicious content  from HTTP
 tshark -n -r securitynik_kaieteur_falls.pcap -T fields -e http.request.method -e http.host -e http.request.uri | sort | uniq
 
+pwd 
+ls -lai 
+# tshark --export-objects http,/dir/ -q
 
+
+echo "===================================================================================="
 echo "===================================================================================="
