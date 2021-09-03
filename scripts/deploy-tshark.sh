946b6149ce456362ebc0ad921abffa2a0d882163
Your Name <you@example.com>
2021-09-03 11:18:44 +0300

    export-objects

--- scripts/deploy-tshark.sh
+++ scripts/deploy-tshark.sh
@@ -16,16 +16,16 @@ echo "==========================================================================
 echo "============================pcap fingerpointing========================================================"
 capinfos securitynik_kaieteur_falls.pcap  #generate a long form report
 
-# capinfos -T securitynik_kaieteur_falls.pcap #generate a TAB delimited table form report
+capinfos -T securitynik_kaieteur_falls.pcap #generate a TAB delimited table form report
 
-# capinfos -TmQ securitynik_kaieteur_falls.pcap >securitynik_kaieteur_falls.csv #generate a CSV style table form report
-# stat  securitynik_kaieteur_falls.csv
+capinfos -TmQ securitynik_kaieteur_falls.pcap >securitynik_kaieteur_falls.csv #generate a CSV style table form report
+stat  securitynik_kaieteur_falls.csv
 
-# editcap -F pcapng  -c 20  securitynik_kaieteur_falls.pcap  securitynik_kaieteur_falls_split.pcap
 editcap -F pcapng  -c 20  securitynik_kaieteur_falls.pcap  /tmp/securitynik_kaieteur_falls_split.pcap
-
 ls -lai /tmp/
-# capinfos securitynik_kaieteur_falls_split.pcap #generate a long form report
+# securitynik_kaieteur_falls_split_00000_20190120175303.pcap
+# securitynik_kaieteur_falls_split_0000X_XXXXXXXXXXXXXX.pcap
+# capinfos securitynik_kaieteur_falls_split_0000X_XXXXXXXXXXXXXX.pcap #generate a long form report
 
 echo "===================================================================================="
 echo "============================tshart analysis========================================================"
@@ -38,11 +38,13 @@ tshark -n -r securitynik_kaieteur_falls.pcap -T fields -e http.request.method -e
 
 pwd 
 ls -lai 
-# tshark --export-objects http,/dir/ -q
-# tshark -n -r securitynik_kaieteur_falls.pcap --export-objects http,/dir/
+tshark --export-objects http,/tmp/ -q
+ls -lai
+# tshark -n -r securitynik_kaieteur0_falls.pcap --export-objects http,/tmp/
 # ls -lai
 # file kaieteur_falls.jpeg
 # stat kaieteur_falls.jpeg
+# clamscan kaieteur_falls.jpeg
 
 echo "===================================================================================="
 echo "===================================================================================="
